d8531ec49a407157de578ac5239ed8355a84c716
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-10-09 18:38:28 +0530

    Create MiddleNodeInLL.cpp

---
+++ Easy/Linked List/MiddleNodeInLL.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+
+        ListNode *fast = head;
+        ListNode *slow = fast;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
